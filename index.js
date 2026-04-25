563796d987f39c153da0dca3b0399f9393256851
Jose Leos <joseleos@gmail.com>
2026-04-25 13:18:27 -0500

    feat: add farewell function

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ function greet(name = 'world') {
   return `Hello, ${name}!`;
 }
 
+function farewell(name = 'world') {
+  return `Goodbye, ${name}!`;
+}
+
 console.log(greet());
+console.log(farewell());
