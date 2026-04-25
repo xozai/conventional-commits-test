b35bf0005ee5123ddc7602f865e0aeba4faef4d5
Jose Leos <joseleos@gmail.com>
2026-04-25 13:40:22 -0500

    feat: add repeat function

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ function whisper(message) {
   return message.toLowerCase();
 }
 
+function repeat(message, times = 2) {
+  return Array(times).fill(message).join(' ');
+}
+
 console.log(greet());
 console.log(farewell());
 console.log(shout(greet('Claude')));
 console.log(whisper(farewell('Claude')));
+console.log(repeat(greet('Claude')));
