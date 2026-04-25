0a413be45168e31f0c6d0dd141daec9f083fd7a5
Jose Leos <joseleos@gmail.com>
2026-04-25 13:25:42 -0500

    feat: add shout function

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ function farewell(name = 'world') {
   return `Goodbye, ${name}!`;
 }
 
+function shout(message) {
+  return message.toUpperCase();
+}
+
 console.log(greet());
 console.log(farewell());
+console.log(shout(greet('Claude')));
